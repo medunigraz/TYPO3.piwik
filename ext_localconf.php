83a98e84afc8d47b8f0f94ca0df9d52fc2a557b5
Michael Fladischer <FladischerMichael@fladi.at>
2016-11-24 10:14:45 +0100

    Remove file inclusion hacks and rely soley on autoloader.

--- ext_localconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("TYPO3_MODE")) die ("Access denied.");
-
-if (TYPO3_MODE == 'FE') {
-	require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('piwik') . 'Classes/UserFunc/Footer.php');
-}
